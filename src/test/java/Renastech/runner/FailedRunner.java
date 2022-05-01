44936786400d815f3e59039a628a907d2dcc300f
sararad12 <sjamalirad@gmail.com>
2022-05-01 19:36:17 -0400

    lab06

---
+++ src/test/java/Renastech/runner/FailedRunner.java
@@ -0,0 +1,33 @@
+package Renastech.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "@target/rerun.txt",
+        glue = "renastech/stepDefinitions",
+        dryRun = true,
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+
+        }
+
+)
+public class FailedRunner {
+
+}
+
+
+
+
+
+
+
+
+
